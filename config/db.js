df7336ce04ee03f532f6be52af9667da4fdd5e2a
Andrew Couture <coutureandrew1@gmail.com>
2023-06-19 23:14:55 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const DB = {
-  URI: 'mongodb+srv://<basicuser123>:<cPt8z8VBnACzv>@cluster0.eqzcdrt.mongodb.net/User?retryWrites=true&w=majority',
+  URI: 'mongodb+srv://basicuser123:cPt8z8VBnACzv@cluster0.mongodb.net/User',
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
 };
+
+module.exports = DB;
