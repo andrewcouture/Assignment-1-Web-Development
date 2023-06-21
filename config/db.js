ef15f4f49a4241c3edf4e6472795d233f87fd467
Andrew Couture <coutureandrew1@gmail.com>
2023-06-20 21:58:34 -0300

    Updated mongo

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const DB = {
-  URI: 'mongodb+srv://basic123:<password>@cluster0.mongodb.net/<database>?retryWrites=true&w=majority',
+  //URI: 'mongodb+srv://basic123:cPt8z8VBnACzv@cluster0.mongodb.net/User?retryWrites=true&w=majority',
+  URI: 'mongodb+srv://coutureandrew1:A.c071376180@cluster0.eqzcdrt.mongodb.net/User?retryWrites=true&w=majority',
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true
